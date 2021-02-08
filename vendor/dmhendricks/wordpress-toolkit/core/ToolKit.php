8d6c4c3fac5ec9dbdd6049f3e7f6be283944ad97
zia123 <zia.kcst@gmail.com>
2021-02-08 17:54:12 +0600

    update git

---
+++ vendor/dmhendricks/wordpress-toolkit/core/ToolKit.php
@@ -0,0 +1,56 @@
+<?php
+namespace WordPress_ToolKit;
+
+/**
+ * ToolKit base class
+ *
+ * Loads configuration and sets constants
+ */
+class ToolKit {
+
+  protected static $config;
+
+  function __construct( $args1 = null, $args2 = null )
+  {
+
+    // Set toolkit configuration defaults
+    $defaults = new ConfigRegistry( dirname( __DIR__ ) . '/config.json' );
+    if( $args2 ) $defaults = $defaults->merge( new ConfigRegistry( $args2 ) );
+
+    // Define toolkit version
+    if ( !defined( __NAMESPACE__ . '\VERSION' ) ) define( __NAMESPACE__ . '\VERSION', $defaults->get( 'toolkit-version' ) );
+
+    // Replace default settings with those provided
+    self::$config = $defaults->merge( new ConfigRegistry( $args1 ) );
+
+  }
+
+  /**
+    * Append a field prefix as defined in $config
+    *
+    * @param string|null $field_name The string/field to prefix
+    * @param string $before String to add before the prefix
+    * @param string $after String to add after the prefix
+    * @return string Prefixed string/field value
+    * @since 0.1.0
+    */
+  public static function prefix( $field_name = null, $before = '', $after = '_' ) {
+
+    $prefix = $before . self::$config->get( 'prefix' ) . $after;
+    return $field_name !== null ? $prefix . $field_name : $prefix;
+
+  }
+
+  /**
+    * Fetch a config value or entire object config
+    *
+    * @param string|null $key The configuration key path to return. If null,
+    *   returns all config values.
+    * @return string|ConfigRegistry Config key path value or ConfigRegistry object
+    * @since 0.1.4
+    */
+  public function get_config( $key = null) {
+    return self::$config->get( $key );
+  }
+
+}
---
+++ wpmembership.php
@@ -0,0 +1,68 @@
+<?php
+
+if( !defined( 'ABSPATH' ) ) die();
+require( __DIR__ . '/vendor/autoload.php' );
+require( __DIR__ . '/app/Settings_Page.php' );
+
+class Licensing_Addon_Example_Plugin {
+
+public $statusmESSAGE;
+
+  function __construct() {
+
+    // Validate license
+    $license_check = new \WordPress_ToolKit\Licensing\WHMCS_License( __DIR__ . '/plugin.json', array( 'plugin' => array( 'path' => __DIR__ ) ) );
+
+    $result = $license_check->validate( $this->get_plugin_option( $license_check->get_config( 'prefix' ) . '_license_key', $license_check->get_config( 'prefix' ) . '_options' ), get_option( $license_check->get_config( 'prefix' ) . '_local_key' ) );
+    if( isset( $result['remotecheck'] ) ) update_option( $license_check->get_config( 'prefix' ) . '_local_key', isset( $result['localkey'] ) ? $result['localkey'] : '' );
+
+    // Load settings page
+    new \Licensing_Example\Settings_Page( $license_check->get_config(), $result['status'] );
+  
+	$this->statusmESSAGE = $result['status'];
+
+    // Run plugin logic - in this example, we'll create a [hello_world] shortcode
+    if ( ! shortcode_exists( 'hello_world' ) ) {
+        add_shortcode( 'hello_world', array( $this, 'hello_world_shortcode' ) );
+    }
+	
+	add_action( 'admin_notices', array( $this, 'show_admin_notice' ) );
+  }
+
+  /**
+    * Get plugin option, with object caching (if available).
+    *
+    * @param string $key The name of the option key
+    * @return mixed The value of specified option key
+    * @link https://github.com/tareq1988/wordpress-settings-api-class WPSAC options
+    */
+  private function get_plugin_option( $key, $group, $cache = true ) {
+
+    $options = get_option( $group );
+    return isset( $options[ $key ] ) ? $options[ $key ] : null;
+
+  }
+
+  /**
+    * Display admin notice on license check failure
+    */
+  public function show_admin_notice() {
+
+     if( $this->statusmESSAGE!=='Active'){
+        $class = 'notice notice-error';
+  	$message = 'Invalid license. Response: ' . $this->statusmESSAGE;
+
+  	printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), $message );
+      }
+  }
+
+  /**
+    * Create a simple [hello_world] shortcode.
+    */
+  public function hello_world_shortcode() {
+    return "Hello World!";
+  }
+
+}
+
+new Licensing_Addon_Example_Plugin();
